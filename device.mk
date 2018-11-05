b94302aa2767b36dcfb1043b49d9f88b288c2e73
Cruzer1 <stegazardd@cruzer.us-east1-b.c.rom-test-221317.internal>
2018-11-05 07:31:10 +0000

    Add some missing function to fux

--- device.mk
+++ device.mk
@@ -69,6 +69,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@3.2-impl \
+   camera.device@1.0-impl \
     camera.msm8916 \
     libmm-qcamera \
     Snap
@@ -86,6 +87,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
  PRODUCT_PACKAGES += \
      YUDoze
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl
 
 # GPS
 PRODUCT_PACKAGES += \
